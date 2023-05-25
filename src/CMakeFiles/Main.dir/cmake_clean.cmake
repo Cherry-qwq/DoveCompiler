dee28de3da447d63ee1b6b3b85fe9c70fcc6518e
Cherry-qwq <20281284@bjtu.edu.cn>
2023-05-25 20:13:26 +0800

    nothing

---
+++ src/CMakeFiles/Main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/Main"
+  "../bin/Main.pdb"
+  "CMakeFiles/Main.dir/main.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
