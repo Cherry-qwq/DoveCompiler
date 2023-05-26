c037ebfcd453a0a9654147dd0ee64d9a1a563b86
npofsi | N/P硅 <npofsi@outlook.com>
2023-05-26 14:38:37 +0800

    Refactor: Clean up

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cherry/下载/DoveCompiler-main
-# Build directory: /home/cherry/下载/DoveCompiler-main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
