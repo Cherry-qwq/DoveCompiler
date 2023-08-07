e7a0736f35d92bb92eb70c10f9cc3fcfaac070aa
StartBear0 <121701580+StartBear0@users.noreply.github.com>
2023-08-07 11:20:14 +0800

    test (#12)
    * Feat: Add types.
    
    * faet : debug++.

--- src/IR/MemoryAccess.h
+++ src/IR/MemoryAccess.h
@@ -80,7 +80,7 @@ namespace ir
     class Store : public Instruction
     {
     public:
-        Store(std::shared_ptr<Value> val, std::shared_ptr<Value> ptr) : Instruction(MakePrimitiveDataType(PrimitiveDataType::TypeID::Void), "Store", 2), val_(val, this), ptr_(ptr, this){};
+        Store(std::shared_ptr<Value> val, std::shared_ptr<Value> ptr) : Instruction(MakePrimitiveDataType(PrimitiveDataType::TypeID::Void), "Store", 2), val_(val, this), ptr_(ptr, this){value_type_=ValueType::Store;};
 
         std::string dump(DumpHelper &helper) const override
         {
